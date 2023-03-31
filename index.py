e4ebdd2a90baa09d14f6973a0fc7e92032ceb41f
Aditya Dubey <49934788+dwivediaditya@users.noreply.github.com>
2023-03-31 12:36:45 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,4 @@
+# Grab the indexes of the facial landamarks for the left and right eye respectively 
+(lstart, lend) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rstart, rend) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(mstart, mend) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
